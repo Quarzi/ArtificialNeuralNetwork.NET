bbca44eaa1458f1d495f957a9e78a565742b2e6a
Quizzarex <kent.stark.olsen@gmail.com>
2015-04-13 19:33:49 +0200

    Changed such Plotting is compatible with new ANN design (Trainer implemented)

--- PlottingError/Form1.Designer.cs
+++ PlottingError/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -61,7 +62,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(966, 41);
+            this.button2.Location = new System.Drawing.Point(1047, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -69,11 +70,20 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(966, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(156, 459);
+            this.listBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1051, 516);
+            this.ClientSize = new System.Drawing.Size(1132, 516);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
@@ -89,6 +99,7 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- PlottingError/Form1.cs
+++ PlottingError/Form1.cs
@@ -16,7 +16,6 @@ namespace PlottingError
     public partial class Form1 : Form
     {
         ArtificialNeuralNetwork.ArtificialNeuralNetwork ann = new ArtificialNeuralNetwork.ArtificialNeuralNetwork(2, 3, 3, 1);
-        ArtificialNeuralNetwork.ArtificialNeuralNetwork ann1 = new ArtificialNeuralNetwork.ArtificialNeuralNetwork(1, 10, 5, 1);
 
         public Form1()
         {
@@ -25,91 +24,115 @@ namespace PlottingError
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //  Generate data matrices for training of ANN as XOR gate
-            Console.WriteLine("\n Training ANN ...\n");
+            ////  Generate data matrices for training of ANN as XOR gate
+            //Console.WriteLine("\n Training ANN ...\n");
 
-            Random rand = new Random((int)DateTime.Now.ToBinary());
-            const int numberOfInputSequences = 16;
-            double[,] inputMatrix = new double[2, numberOfInputSequences], targetMatrix = new double[1, numberOfInputSequences];
-            int x1, x2;
+            //Random rand = new Random((int)DateTime.Now.ToBinary());
+            //const int numberOfInputSequences = 16;
+            //double[,] inputMatrix = new double[2, numberOfInputSequences], targetMatrix = new double[1, numberOfInputSequences];
+            //int x1, x2;
 
-            for (int i = 0; i < numberOfInputSequences; i++)
-            {
-                x1 = rand.Next(0, 2);
-                x2 = rand.Next(0, 2);
+            //for (int i = 0; i < numberOfInputSequences; i++)
+            //{
+            //    x1 = rand.Next(0, 2);
+            //    x2 = rand.Next(0, 2);
 
-                inputMatrix[0, i] = x1;
-                inputMatrix[1, i] = x2;
-                targetMatrix[0, i] = x1 ^ x2;
-            }
+            //    inputMatrix[0, i] = x1;
+            //    inputMatrix[1, i] = x2;
+            //    targetMatrix[0, i] = x1 ^ x2;
+            //}
 
-            double[] errorDev = this.ann.AnnTrainingOnline(inputMatrix, targetMatrix, 4, 10000);
+            //double[] errorDev = this.ann.AnnTrainingOnline(inputMatrix, targetMatrix, 4, 10000);
 
-            //  Plot
-            this.chart1.Series.Clear();
+            ////  Plot
+            //this.chart1.Series.Clear();
 
-            Series serie = new Series("Error");
-            serie.ChartType = SeriesChartType.Line;
-            //serie.Points.Add(errorDev);
-            for (int i = 0; i < errorDev.Length; i++)
-            {
-                serie.Points.Add(new DataPoint(i, errorDev[i]));
-            }
+            //Series serie = new Series("Error");
+            //serie.ChartType = SeriesChartType.Line;
+            ////serie.Points.Add(errorDev);
+            //for (int i = 0; i < errorDev.Length; i++)
+            //{
+            //    serie.Points.Add(new DataPoint(i, errorDev[i]));
+            //}
 
 
-            this.chart1.Series.Add(serie);
+            //this.chart1.Series.Add(serie);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //  Generate data matrices for training of ANN as XOR gate
-            Random rand = new Random((int)DateTime.Now.ToBinary());
-            const int numberOfInputSequences = 16;
-            double[,] inputMatrix = new double[1, numberOfInputSequences], targetMatrix = new double[1, numberOfInputSequences];
-            double x1 = -8;
+            const double minX = -5, maxX = 5, stepSize = 0.001, trainingStepSize = Math.PI / 5;
+            const int numberOfTrainingSteps = (int)((maxX - minX) / trainingStepSize) + 1;
+            const int hiddenLayers = 3, neuronsPerHiddenLayer = 5;
+            double[,] inputMatrix = new double[1, numberOfTrainingSteps], targetMatrix = new double[1, numberOfTrainingSteps];
+            int index = 0;
+            double x, y, est;
 
-            for (int i = 0; i < numberOfInputSequences; i++, x1++)
+            ChartArea errorChart = new ChartArea("ErrorChart"), outputChart = new ChartArea("OutputChart");
+            Series errorSequence = new Series("Errors"), targetSequence = new Series("Target"), outputSequence = new Series("Output");
+            
+            errorSequence.ChartArea = errorChart.Name;
+            errorSequence.Color = Color.Red;
+            errorSequence.ChartType = SeriesChartType.Line;
+
+            targetSequence.ChartArea = outputChart.Name;
+            targetSequence.Color = Color.Green;
+            targetSequence.ChartType = SeriesChartType.Line;
+
+            outputSequence.ChartArea = outputChart.Name;
+            outputSequence.Color = Color.Red;
+            outputSequence.ChartType = SeriesChartType.Line;
+
+            //  Generate input and target matrices for training of ANN
+            for (x = minX; x < maxX; x += trainingStepSize)
             {
-                inputMatrix[0, i] = x1;
-                targetMatrix[0, i] = Math.Pow(x1, 3);
+                y = Math.Sin(x); 
+
+                inputMatrix[0, index] = x;
+                targetMatrix[0, index] = y;
+                index++;
             }
 
-            double[] errorDev = this.ann1.AnnTrainingOnline(inputMatrix, targetMatrix, 4, 10000);
+            double scale = targetMatrix.Max() - targetMatrix.Min();
+            targetMatrix = targetMatrix.Multiply(1 / scale);
+
+            //  Train ANN
+            ArtificialNeuralNetwork.ArtificialNeuralNetwork ann = new ArtificialNeuralNetwork.ArtificialNeuralNetwork(inputMatrix.Rows(), hiddenLayers, neuronsPerHiddenLayer, targetMatrix.Rows());
+            double[] errorDev = ann.Trainer.TrainAnn(inputMatrix, targetMatrix);
+
+            //  Add errors to listbox
+            this.listBox1.Items.Clear();
+            foreach (var item in errorDev)
+            {
+                this.listBox1.Items.Add(item);
+            }
+
+            //  Extract relevant data
+            for (int i = 0; i < errorDev.Length; i++)
+            {
+                errorSequence.Points.Add(new DataPoint(i, errorDev[i]));
+            }
+
+            for (x = minX; x <= maxX; x += stepSize)
+            {
+                //y = 2 * Math.Pow(x, 3) + 2 / scale;
+                y = Math.Sin(x);
+
+                double[] yy = ann.FeedForward(new double[] { x }).Multiply(scale);
+                outputSequence.Points.Add(new DataPoint(x, yy));
+                targetSequence.Points.Add(new DataPoint(x, y));
+            }
 
             //  Plot
             this.chart1.Series.Clear();
+            this.chart1.ChartAreas.Clear();
 
-            Series serie = new Series("Error");
-            serie.ChartType = SeriesChartType.Line;
-            //serie.Points.Add(errorDev);
-            for (int i = 0; i < errorDev.Length; i++)
-            {
-                serie.Points.Add(new DataPoint(i, errorDev[i]));
-            }
+            this.chart1.ChartAreas.Add(errorChart);
+            this.chart1.ChartAreas.Add(outputChart);
 
-            //  Generate data-points from ANN
-            double[] y;
-            Series annOutput = new Series("ANN Output"), target = new Series("Target");
-            serie.ChartType = SeriesChartType.Line;
-
-            for (double x = -10; x <= 10; x += 0.001)
-            {
-                y = ann1.FeedForward(new double[] { x });
-                annOutput.Points.Add(new DataPoint(x, y));
-                target.Points.Add(new DataPoint(x, Math.Pow(x, 3)));
-            }
-
-            this.chart1.Series.Add(serie);
-            ChartArea cArea = new ChartArea("Function");
-            annOutput.ChartArea = "Function";
-            annOutput.ChartType = SeriesChartType.Line;
-            target.ChartArea = "Function";
-            target.ChartType = SeriesChartType.Line;
-
-            this.chart1.ChartAreas.Add(cArea);
-
-            this.chart1.Series.Add(annOutput);
-            this.chart1.Series.Add(target);
+            this.chart1.Series.Add(errorSequence);
+            this.chart1.Series.Add(targetSequence);
+            this.chart1.Series.Add(outputSequence);
         }
     }
 }
