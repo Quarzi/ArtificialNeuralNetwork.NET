4f1d93b6a439d705ac91d6e2c2a69e79bee92e62
Quizzarex <kent.stark.olsen@gmail.com>
2015-04-14 18:20:52 +0200

    Fixed dependency bug

--- ArtificialNeuralNetworkVisualizer/Main.cs
+++ ArtificialNeuralNetworkVisualizer/Main.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
 using ArtificialNeuralNetwork;
+using ArtificialNeuralNetwork.Extensions;
 using ANN = ArtificialNeuralNetwork;
 
 namespace ArtificialNeuralNetworkVisualizer
