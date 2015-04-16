4569eb5aa34ecf5f21ffa49c36284dfbb9ccf4c7
Quarzi <kim.jan.andersen82@gmail.com>
2015-04-16 20:40:55 +0200

    added syncfysion controls

--- ArtificialNeuralNetworkVisualizerMVC/Controllers/VisualizerController.cs
+++ ArtificialNeuralNetworkVisualizerMVC/Controllers/VisualizerController.cs
@@ -3,30 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Syncfusion.Core;
+using Syncfusion.MVC.EJ;
+using System.Web.Mvc.Html;
+using ArtificialNeuralNetworkVisualizerMVC.Models;
+
 
 namespace ArtificialNeuralNetworkVisualizerMVC.Controllers
 {
     public class VisualizerController : Controller
     {
-        // GET: Visualizer
+        // GET: VisualizerViewModel
         public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult ToolBox()
+        {
+            return View(new VisualizerSettings());
+        }
+        public ActionResult GrafView()
         {
             return View();
         }
 
-        // GET: Visualizer/Details/5
+        // GET: VisualizerViewModel/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Visualizer/Create
+        // GET: VisualizerViewModel/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Visualizer/Create
+        // POST: VisualizerViewModel/Create
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
@@ -42,13 +57,13 @@ namespace ArtificialNeuralNetworkVisualizerMVC.Controllers
             }
         }
 
-        // GET: Visualizer/Edit/5
+        // GET: VisualizerViewModel/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: Visualizer/Edit/5
+        // POST: VisualizerViewModel/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
@@ -64,13 +79,13 @@ namespace ArtificialNeuralNetworkVisualizerMVC.Controllers
             }
         }
 
-        // GET: Visualizer/Delete/5
+        // GET: VisualizerViewModel/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Visualizer/Delete/5
+        // POST: VisualizerViewModel/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
