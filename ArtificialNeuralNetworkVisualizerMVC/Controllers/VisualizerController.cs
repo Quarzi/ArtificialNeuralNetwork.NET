7bf833aa8b922e778a6bdec7d50f275221cea516
Quarzi <kim.jan.andersen82@gmail.com>
2015-04-17 13:23:24 +0200

    Updated the new mvc project with the old content

---
+++ ArtificialNeuralNetworkVisualizerMVC/Controllers/VisualizerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using ArtificialNeuralNetworkVisualizerMVC.Models;
+
+namespace ArtificialNeuralNetworkVisualizerMVC.Controllers
+{
+    public class VisualizerController : Controller
+    {
+        // GET: Visualizer
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult ToolBox()
+        {
+            return View(new VisualizerSettings());
+        }
+        public ActionResult GrafView()
+        {
+            return View();
+        }
+    }
+}
