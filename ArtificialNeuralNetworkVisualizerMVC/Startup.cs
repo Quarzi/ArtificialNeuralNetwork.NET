659050702ba97e5e921293ba46907c056a832589
Quarzi <kim.jan.andersen82@gmail.com>
2015-04-17 02:54:11 +0200

    Added new updated Mvc project

---
+++ ArtificialNeuralNetworkVisualizerMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArtificialNeuralNetworkVisualizerMVC.Startup))]
+namespace ArtificialNeuralNetworkVisualizerMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
